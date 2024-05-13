92fae5267032a1d86d39caac261f82d02605b1d4
fryedericharielly <135351078+fryedericharielly@users.noreply.github.com>
2024-05-13 14:08:19 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,15 @@
+let Fundo;
+let imagemCarro;
+let imagemCarro1;
+let imagemCarro2;
+let imagemAtor;
+
+
+ function preload(){
+  Fundo = loadImage("imagens/pista rum (1).png");
+  imagemCarro = loadImage("imagens/carropreto-1.png");
+   imagemCarro1 = loadImage("imagens/carro.vermelho-1.png");
+   imagemCarro2 = loadImage("imagens/carrobranco-1.png");
+   imagemAtor = loadImage("imagens/ovelha.png");
+  imagemCarros = [imagemCarro, imagemCarro1, imagemCarro2,imagemCarro, imagemCarro1, imagemCarro2];   
+  }
